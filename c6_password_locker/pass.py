bba65710161ae616036eba4aad8178c1b36a3bfa
neex-io <neex.io@protonmail.com>
2018-01-03 13:07:51 -0800

    Copy password for given account to clipboard

---
+++ c6_password_locker/pass.py
@@ -0,0 +1,27 @@
+# Very insecure password storage for educational purpose only
+# requires xsel utility
+# sudo apt install xsel
+
+
+#! python3
+# pass.py
+
+import pyperclip
+import sys
+
+PASSWORDS = {"email": "ZW0C2XUfQKnmGiN3Fc2LX55Jb",
+             "blog": "nghIz
+             QCDsqJ5r7VKp8f4h",
+             "luggage": "672392"}
+
+if len(sys.argv) < 2:
+    print("Usage: python pass.py [account] = copy account pasword")
+    sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print("Password for account: [{}] copied to clipboard!".format(account))
+else:
+    print("There is no account: {}".format(account))
